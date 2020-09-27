a98910c6298448f1c533e4a28bb1a30944968ce8
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-27 11:24:30 -0400

    hello

---
+++ test.js
@@ -0,0 +1,24 @@
+// +! HEROKU COMMANDS
+
+// +& check the logs
+// ++ heroku logs --tail --app=<appname>
+
+// +& run bash from your projects server
+// ++ heroku run bash --app=<appname>
+
+// +! GIT COMMANDS
+
+// +& Add all files in current folder and below
+// ++ git add .
+
+// +& commit with message
+// ++ git commit -m "message"
+
+// +& add a remote repository
+// ++ git remote add <name> <url>
+
+// +& remove a remote
+// ++ git remote rm <name>
+
+// +& push a branch to a particular remote
+// ++ git push <remoteName> <branchName>
